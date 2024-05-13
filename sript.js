57eadd4da42c66e3e1e6e8f5f9e2c2253d2224d4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-13 19:55:28 +0530

    added event listener to submit button and a arrow function

--- sript.js
+++ sript.js
@@ -3,3 +3,7 @@ var studentName =document.getElementById('studentName');
 var email = document.getElementById('email');
 var phone = document.getElementById('phone');
 var submit = document.getElementById('submit');
+
+submit.addEventListener('click',()=>{
+    
+})
