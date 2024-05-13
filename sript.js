dc5192673a7c893f65132b162dcb9e9b9c6b378e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-13 19:51:27 +0530

    selected elements of html and stored in var;added id to submit button

---
+++ sript.js
@@ -0,0 +1,5 @@
+var studentId = document.getElementById('studentId');
+var studentName =document.getElementById('studentName');
+var email = document.getElementById('email');
+var phone = document.getElementById('phone');
+var submit = document.getElementById('submit');
